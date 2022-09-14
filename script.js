c4b3ddd2a6dbe6b563f389442ec9ec3af8bbd299
MahamudaM <mahamuda3052@gmail.com>
2022-09-15 04:50:30 +0600

    start button counting fix

--- script.js
+++ script.js
@@ -102,13 +102,13 @@ const closeModal = () => {
 
 const start = () => {
   // If already started, do not start again
-  // if (startTime)return;
+  if (startTime)return;
 
   let count = 3;
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
   
 
   
